db24b938bd6515a7205fc03b4cab635ecee11fd2
maxGross <maxgross@t-online.de>
2015-05-18 15:24:25 +0200

    Grundlegendes

--- src/de/wagnst/tpe/exercise/crypter/IllegalKeyException.java
+++ src/de/wagnst/tpe/exercise/crypter/IllegalKeyException.java
@@ -1,5 +1,15 @@
 package de.wagnst.tpe.exercise.crypter;
 
 public class IllegalKeyException extends Exception {
+    private String key;
+
+    public IllegalKeyException(String message, String key) {
+        super(message);
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
 
 }
--- src/de/wagnst/tpe/exercise/crypter/IllegalMessageException.java
+++ src/de/wagnst/tpe/exercise/crypter/IllegalMessageException.java
@@ -2,4 +2,15 @@ package de.wagnst.tpe.exercise.crypter;
 
 public class IllegalMessageException extends Exception {
 
+    private String userMessage;
+
+    public IllegalMessageException(String message, String userMessage) {
+        super(message);
+        this.userMessage = userMessage;
+    }
+
+    public String getKey() {
+        return userMessage;
+    }
+
 }
