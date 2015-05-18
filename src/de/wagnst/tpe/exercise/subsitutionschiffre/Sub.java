be5932e1f14da352d0b753e6fda5bffc5ca08e7d
maxGross <maxgross@t-online.de>
2015-05-18 15:24:34 +0200

    try catch

--- src/de/wagnst/tpe/exercise/subsitutionschiffre/Sub.java
+++ src/de/wagnst/tpe/exercise/subsitutionschiffre/Sub.java
@@ -34,54 +34,70 @@ class Sub implements Crypter {
     @Override
     public String verschluesseln(String key, String message)
             throws IllegalKeyException, IllegalMessageException {
-
-        String codedMessage = "";
-
-        /* selects the letter to decode */
-        int pointer = 0;
-        /* loop over the whole message */
-        // TODO delete spaces!!
-        for (int i = 0; i < message.length(); i++) {
-            /* start from begin of key */
-            int j = 0;
-            while (message.charAt(pointer) != (cleartext.charAt(j))) {
-                j++;
+        try {
+            if (key.length() < 26) {
+                throw new IllegalKeyException("wrong format at key", key);
             }
-            /* we found the letter in cleartext at position(j) */
-            codedMessage += key.charAt(j);
-            pointer++;
-        }
+            String codedMessage = "";
 
-        return codedMessage;
+            /* selects the letter to decode */
+            int pointer = 0;
+            /* loop over the whole message */
+            // TODO delete spaces!!
+            for (int i = 0; i < message.length(); i++) {
+                /* start from begin of key */
+                int j = 0;
+                while (message.charAt(pointer) != (cleartext.charAt(j))) {
+                    j++;
+                }
+                /* we found the letter in cleartext at position(j) */
+                codedMessage += key.charAt(j);
+                pointer++;
+            }
+
+            return codedMessage;
+
+        } catch (IllegalKeyException e) {
+            System.out.println("falscher Key!");
+        }
     }
 
     @Override
     public String entschluesseln(String key, String cypherText)
             throws IllegalKeyException, IllegalMessageException {
-        String decodedMessage = "";
-
-        /* selects the letter to decode */
-        int pointer = 0;
-        /* loop over the whole message */
-        for (int i = 0; i < cypherText.length(); i++) {
-            /* start from begin of key */
-            int j = 0;
-
-            /* TODO do we need this? */
-            /* may spaces appear */
-            if (cypherText.charAt(pointer) == ' ') {
-                decodedMessage += ' ';
-            } else {
-                while (cypherText.charAt(pointer) != (key.charAt(j))) {
-                    j++;
-                }
-                /* we found the letter in cleartext at position(j) */
-                decodedMessage += cleartext.charAt(j);
+        try {
+            if (key.length() < 26) {
+                throw new IllegalKeyException("wrong format at key", key);
             }
 
-            pointer++;
-        }
+            String decodedMessage = "";
 
-        return decodedMessage;
+            /* selects the letter to decode */
+            int pointer = 0;
+            /* loop over the whole message */
+            for (int i = 0; i < cypherText.length(); i++) {
+                /* start from begin of key */
+                int j = 0;
+
+                /* TODO do we need this? */
+                /* may spaces appear */
+                if (cypherText.charAt(pointer) == ' ') {
+                    decodedMessage += ' ';
+                } else {
+                    while (cypherText.charAt(pointer) != (key.charAt(j))) {
+                        j++;
+                    }
+                    /* we found the letter in cleartext at position(j) */
+                    decodedMessage += cleartext.charAt(j);
+                }
+
+                pointer++;
+            }
+
+            return decodedMessage;
+
+        } catch (IllegalKeyException e) {
+            System.out.println("falscher Key!");
+        }
     }
 }
