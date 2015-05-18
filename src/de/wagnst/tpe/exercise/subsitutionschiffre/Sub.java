2c6345274eaba1ee977131dc05616e1d78048828
maxGross <maxgross@t-online.de>
2015-05-18 14:55:01 +0200

    verschlüsseln und entschlüsseln ohne Exceptions implementiert

--- src/de/wagnst/tpe/exercise/subsitutionschiffre/Sub.java
+++ src/de/wagnst/tpe/exercise/subsitutionschiffre/Sub.java
@@ -1,5 +1,87 @@
 package de.wagnst.tpe.exercise.subsitutionschiffre;
 
-class Sub {
+import de.wagnst.tpe.exercise.crypter.*;
 
+class Sub implements Crypter {
+
+    /*
+     * private char[] cleartext = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+     * 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+     * 'X', 'Y', 'Z' };
+     */
+
+    private String cleartext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String key;
+
+    public Sub() {
+
+    }
+
+    public String getCleartext() {
+        return cleartext;
+    }
+
+    public void setKey(String key) {
+
+        /* key contains exactly 26 letters */
+        if (key.length() == 26) {
+            this.key = key;
+        }
+        // TODO implement the case if the given key contains less than 26
+        // letters
+    }
+
+    @Override
+    public String verschluesseln(String key, String message)
+            throws IllegalKeyException, IllegalMessageException {
+
+        String codedMessage = "";
+
+        /* selects the letter to decode */
+        int pointer = 0;
+        /* loop over the whole message */
+        // TODO delete spaces!!
+        for (int i = 0; i < message.length(); i++) {
+            /* start from begin of key */
+            int j = 0;
+            while (message.charAt(pointer) != (cleartext.charAt(j))) {
+                j++;
+            }
+            /* we found the letter in cleartext at position(j) */
+            codedMessage += key.charAt(j);
+            pointer++;
+        }
+
+        return codedMessage;
+    }
+
+    @Override
+    public String entschluesseln(String key, String cypherText)
+            throws IllegalKeyException, IllegalMessageException {
+        String decodedMessage = "";
+
+        /* selects the letter to decode */
+        int pointer = 0;
+        /* loop over the whole message */
+        for (int i = 0; i < cypherText.length(); i++) {
+            /* start from begin of key */
+            int j = 0;
+
+            /* TODO do we need this? */
+            /* may spaces appear */
+            if (cypherText.charAt(pointer) == ' ') {
+                decodedMessage += ' ';
+            } else {
+                while (cypherText.charAt(pointer) != (key.charAt(j))) {
+                    j++;
+                }
+                /* we found the letter in cleartext at position(j) */
+                decodedMessage += cleartext.charAt(j);
+            }
+
+            pointer++;
+        }
+
+        return decodedMessage;
+    }
 }
--- src/de/wagnst/tpe/exercise/subsitutionschiffre/TestSub.java
+++ src/de/wagnst/tpe/exercise/subsitutionschiffre/TestSub.java
@@ -1,5 +1,29 @@
 package de.wagnst.tpe.exercise.subsitutionschiffre;
 
-class TestSub {
+import org.junit.Test;
+import static org.junit.Assert.*;
+import de.wagnst.tpe.exercise.crypter.*;
 
+public class TestSub {
+                       // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    private String key = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+
+    @Test
+    public void testVerschluessln() {
+
+        Crypter test = new Sub();
+        assertEquals("DEFG", test.verschluesseln(key, "ABCD"));
+        assertEquals("BCD", test.verschluesseln(key, "YZA"));
+        assertEquals("CGP", test.verschluesseln(key, "ZDM"));
+        assertEquals("DEFGHIJKLMNOPQRSTUVWXYZABC",
+                test.verschluesseln(key, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+
+    }
+    
+    @Test 
+    public void testEntschluesseln(){
+        Crypter test = new Sub();
+        assertEquals("ABCD", test.entschluesseln(key, "DEFG"));
+        assertEquals("ZZ BB", test.entschluesseln(key, "CC EE"));
+    }
 }
