5e0ef07956d42c60acf25a1496ff5cb9eba46888
maxGross <maxgross@t-online.de>
2015-05-19 12:22:27 +0200

    Exceptions für falschen Key implementiert und getestet

--- src/de/wagnst/tpe/exercise/subsitutionschiffre/Sub.java
+++ src/de/wagnst/tpe/exercise/subsitutionschiffre/Sub.java
@@ -36,47 +36,64 @@ class Sub implements Crypter {
     @Override
     public String verschluesseln(String key, String message)
             throws IllegalKeyException, IllegalMessageException {
-        try {
-            if (key.length() != 26) {
+
+        /* TODO key does not match with encodeprocedure */
+
+        String codedMessage = "";
+
+        if (key.length() != 26) {
+            throw new IllegalKeyException(
+                    "please insert a key within 26 letters");
+        }
+        /* checks whole key for illegal literals */
+        for (int i = 0; i < key.length(); i++) {
+            if (!cleartext.contains((Character.toString(key.charAt(i))))) {
                 throw new IllegalKeyException(
-                        "please insert a key within 26 letters", key);
+                        "please Only use capital letters for your key");
             }
-            /* TODO key does not match with encodeprocedure */
+        }
 
-            String codedMessage = "";
+        /* searching for duplicate letters */
+        for (int i = 0; i < key.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < key.length(); j++) {
+                if (key.charAt(i) == key.charAt(j)) {
+                    count++;
+                    if (count > 1) {
+                        throw new IllegalKeyException(
+                                "duplicates are not allowed at key");
+                    }
 
-            /* selects the letter to encode */
-            int pointer = 0;
-            /* loop over the whole message */
-            for (int i = 0; i < message.length(); i++) {
-                /* start from begin of key */
-                int j = 0;
-                while (message.charAt(pointer) != (cleartext.charAt(j))) {
-                    j++;
+                }
+            }
+        }
+
+        /* select the letter to encode */
+        int pointer = 0;
+        for (int i = 0; i < message.length(); i++) {
+            /* start from begin of key */
+            int j = 0;
+            while (message.charAt(pointer) != (cleartext.charAt(j))) {
+                j++;
+
+                try {
                     if (j == 26) {
                         /* literal not at cleartext */
                         throw new IllegalMessageException(
                                 "please ONLY use capital letters for your message",
                                 message);
                     }
+                } catch (IllegalMessageException e) {
+                    System.out.println(e.getMessage());
                 }
-                /* we found the letter in cleartext at position(j) */
-                codedMessage += key.charAt(j);
-                pointer++;
             }
-
-            return codedMessage;
-
-        } catch (IllegalKeyException e) {
-            String keyException = e.getMessage();
-            e.printStackTrace();
-        } catch (IllegalMessageException e) {
-            String messageException = e.getMessage();
-            e.printStackTrace();
+            /* we found the letter in cleartext at position(j) */
+            codedMessage += key.charAt(j);
+            pointer++;
         }
 
-        /* TODO WARUM DAS?? Wie geht man damit um? */
-        return "";
+        return codedMessage;
+
     }
 
     /**
@@ -92,43 +109,33 @@ class Sub implements Crypter {
      *         unerlaubte Zeichen enthält.
      */
     @Override
-    public String entschluesseln(String key, String cypherText)
-            throws IllegalKeyException, IllegalMessageException {
-        try {
-            if (key.length() < 26) {
-                throw new IllegalKeyException("wrong format at key", key);
-            }
+    public String entschluesseln(String key, String cypherText) {
 
-            String decodedMessage = "";
+        String decodedMessage = "";
 
-            /* selects the letter to decode */
-            int pointer = 0;
-            /* loop over the whole message */
-            for (int i = 0; i < cypherText.length(); i++) {
-                /* start from begin of key */
-                int j = 0;
+        /* selects the letter to decode */
+        int pointer = 0;
+        /* loop over the whole message */
+        for (int i = 0; i < cypherText.length(); i++) {
+            /* start from begin of key */
+            int j = 0;
 
-                /* TODO do we need this? */
-                /* may spaces appear */
-                if (cypherText.charAt(pointer) == ' ') {
-                    decodedMessage += ' ';
-                } else {
-                    while (cypherText.charAt(pointer) != (key.charAt(j))) {
-                        j++;
-                    }
-                    /* we found the letter in cleartext at position(j) */
-                    decodedMessage += cleartext.charAt(j);
+            /* TODO do we need this? */
+            /* may spaces appear */
+            if (cypherText.charAt(pointer) == ' ') {
+                decodedMessage += ' ';
+            } else {
+                while (cypherText.charAt(pointer) != (key.charAt(j))) {
+                    j++;
                 }
-
-                pointer++;
+                /* we found the letter in cleartext at position(j) */
+                decodedMessage += cleartext.charAt(j);
             }
 
-            return decodedMessage;
-
-        } catch (IllegalKeyException e) {
-            System.out.println("falscher Key!");
+            pointer++;
         }
-        /* TODO WARUM DAS?? Wie geht man damit um? */
-        return "";
+
+        return decodedMessage;
+
     }
 }
--- src/de/wagnst/tpe/exercise/subsitutionschiffre/TestSub.java
+++ src/de/wagnst/tpe/exercise/subsitutionschiffre/TestSub.java
@@ -7,9 +7,14 @@ import de.wagnst.tpe.exercise.crypter.*;
 
 public class TestSub {
     // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
     private String key = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+
+    /* TODO key mit doppelten Zeichen; */
+
     Crypter test = new Sub();
 
+    /* encode */
     @Test
     public void testVerschluessln() throws IllegalKeyException,
             IllegalMessageException {
@@ -22,17 +27,80 @@ public class TestSub {
 
     }
 
+    /*
+     * KeyExceptions too small/large key
+     */
+
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException1() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("DE", "HALLO");
+    }
+
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException2() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("DEFGHIJKLMNOPQRSTUVWXYZABCX", "HALLO");
+    }
+
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException5() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("DEFGHIJKLMNOPQRSTUVWXYZAB", "HALLO");
+    }
+
+    /* illegal literals */
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException3() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("?", "HALLO");
+    }
+
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException4() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("a", "HALLO");
+    }
+
+    /* duplicates */
+    /* begin */
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException6() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("DDEFGHIJKLMNOPQRSTUVWXYZABC", "HALLO");
+    }
+
+    /* middle */
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException7() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("DEFGHIJKLMMOPQRSTUVWXYZABC", "HALLO");
+    }
+
+    /* end */
+    @Test(expected = IllegalKeyException.class)
+    public void IllegalKeyException8() throws IllegalKeyException,
+            IllegalMessageException {
+
+        test.verschluesseln("DEFGHIJKLMNOPQRSTUVWXYZACC", "HALLO");
+    }
+
+    /* MessageExceptions */
+
+    /* decode */
     @Test
     public void testEntschluesseln() throws IllegalKeyException,
             IllegalMessageException {
 
         assertEquals("ABCD", test.entschluesseln(key, "DEFG"));
         assertEquals("ZZ BB", test.entschluesseln(key, "CC EE"));
-    }
-
-    @Test(expected = IllegalMessageException.class)
-    public void testIllegalMessageException() {
-        new Sub().verschluesseln(key, "A B");
     }
 
 }
