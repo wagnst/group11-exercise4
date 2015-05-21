99bc5c202ea8d04ac9e31c2f47cf0d8d2520227f
wagnst <mail@steffenwagner.com>
2015-05-21 12:19:07 +0200

    added first implementation of xor crypt class

---
+++ src/de/wagnst/tpe/exercise/xorcrypt/XorCrypt.java
@@ -0,0 +1,32 @@
+package de.wagnst.tpe.exercise.xorcrypt;
+
+import de.wagnst.tpe.exercise.crypter.Crypter;
+import de.wagnst.tpe.exercise.crypter.IllegalKeyException;
+import de.wagnst.tpe.exercise.crypter.IllegalMessageException;
+
+/**
+ * Copyright by Steffen Wagner (D059727)
+ *
+ * Created on 21.05.2015, 12:13 Project: group11-exercise4
+ */
+
+public class XorCrypt implements Crypter {
+
+    /**
+     * @see de.wagnst.tpe.exercise.crypter
+     */
+    @Override
+    public String verschluesseln(String key, String message)
+            throws IllegalKeyException, IllegalMessageException {
+
+    }
+
+    /**
+     * @see de.wagnst.tpe.exercise.crypter
+     */
+    @Override
+    public String entschluesseln(String key, String cypherText)
+            throws IllegalKeyException, IllegalMessageException {
+
+    }
+}
