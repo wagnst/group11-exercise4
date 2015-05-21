6804dea4ef0bf5664a9650272d54056b65e3f950
Rahtainka <rahtainka@t-online.de>
2015-05-21 13:33:18 +0200

    changed name of CeasarCrypt to CrypterCeaser and added methods

---
+++ src/de/wagnst/tpe/exercise/ceasarverschluesselung/CrypterCaesar.java
@@ -0,0 +1,21 @@
+package de.wagnst.tpe.exercise.ceasarverschluesselung;
+
+
+import de.wagnst.tpe.exercise.crypter.Crypter;
+import de.wagnst.tpe.exercise.crypter.IllegalKeyException;
+import de.wagnst.tpe.exercise.crypter.IllegalMessageException;
+
+public class CrypterCaesar implements Crypter{
+
+
+
+    @Override
+    public String entschluesseln(String key, String cypherText) throws IllegalKeyException, IllegalMessageException {
+        return null;
+    }
+
+    @Override
+    public String verschluesseln(String key, String message) throws IllegalKeyException, IllegalMessageException {
+        return null;
+    }
+}
