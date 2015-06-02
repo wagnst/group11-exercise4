3d40e622ec8d287de3a7910c8a31e32e0eca3c29
wagnst <mail@steffenwagner.com>
2015-06-02 12:24:36 +0200

    removed old UI

--- src/de/wagnst/tpe/exercise/swingOberflaeche/Main.java
+++ src/de/wagnst/tpe/exercise/swingOberflaeche/Main.java
@@ -5,13 +5,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        JFrame cryptoFenster = new HauptansichtDoof();
-        cryptoFenster.setSize(500, 300); // window position
-        cryptoFenster.setLocationRelativeTo(null); // set it visible
-        cryptoFenster.setVisible(true);
-        */
-
         /* setting for Enigma */
         JFrame cryptoFenster = new Enigma();
         cryptoFenster.setSize(700, 400);
