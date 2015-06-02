c2c5c31a405748ccda0e7f88a45f24fe1f48cb84
wagnst <mail@steffenwagner.com>
2015-06-02 12:19:44 +0200

    jdoc for caesar class

--- src/de/wagnst/tpe/exercise/crypters/CrypterCaesar.java
+++ src/de/wagnst/tpe/exercise/crypters/CrypterCaesar.java
@@ -2,20 +2,27 @@ package de.wagnst.tpe.exercise.crypters;
 
 import de.wagnst.tpe.exercise.crypter.*;
 
-/*TODO JavaDoc Klassenbeschreibung*/
+/**
+ * Class to encode and decode a message with a key. Using method of
+ * Caesar chiffre. Implements Crypter interface.
+ *
+ * @author Rahtainka
+ * @see <a href="https://github.com/tpe-lecture/group11-exercise4#caesar-verschl%C3%BCsselung">GitHub description</a>
+ */
+
 class CrypterCaesar implements Crypter {
 
 
     /**
      * Decode the encoded message with the key.
      *
-     * @param key        key, which should be used.
-     * @param cypherText message, which should be encoded.
-     * @return decoded message.
-     * @throws IllegalKeyException     will throw, if the key doesn't fit to the
-     *                                 ciphering method.
-     * @throws IllegalMessageException will throw, if the message has forbidden
-     *                                 signs.
+     * @param key                       key, which should be used.
+     * @param cypherText                message, which should be encoded.
+     * @return                          decoded message.
+     * @throws IllegalKeyException      will throw, if the key doesn't fit to
+     *                                  the ciphering method.
+     * @throws IllegalMessageException  will throw, if the message has forbidden
+     *                                  signs.
      */
     @Override
     public String entschluesseln(String key, String cypherText)
@@ -47,13 +54,13 @@ class CrypterCaesar implements Crypter {
     /**
      * Encode the decoded message with the key.
      *
-     * @param key     key, which should be used.
-     * @param message message, which should be encoded.
-     * @return decoded message.
-     * @throws IllegalKeyException     will throw, if the key doesn't fit to the
-     *                                 ciphering method.
-     * @throws IllegalMessageException will throw, if the message has forbidden
-     *                                 signs.
+     * @param key                       key, which should be used.
+     * @param message                   message, which should be encoded.
+     * @return                          decoded message.
+     * @throws IllegalKeyException      will throw, if the key doesn't fit to
+     *                                  the ciphering method.
+     * @throws IllegalMessageException  will throw, if the message has forbidden
+     *                                  signs.
      */
 
     @Override
