e8f6e48e3523688dc7265a24a9ec96a6a57d6b55
wagnst <mail@steffenwagner.com>
2015-06-07 19:36:15 +0200

    initial encode tests for xor

--- src/de/wagnst/tpe/exercise/tests/TestXOREncode.java
+++ src/de/wagnst/tpe/exercise/tests/TestXOREncode.java
@@ -1,11 +1,22 @@
 package de.wagnst.tpe.exercise.tests;
 
+import de.wagnst.tpe.exercise.crypter.*;
+import de.wagnst.tpe.exercise.crypters.CrypterFactory;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestXOREncode {
 
-    @Test
-    public void testVerschluesseln() throws Exception {
+    private String key = "TPERULES";
 
+    @Test
+    public void encode() throws IllegalKeyException, IllegalMessageException,
+            IllegalCrypterException {
+
+        Crypter test = CrypterFactory.createCrypter(CrypterVerfahren.XOR);
+
+        assertEquals("URFVPJB[]ZN^XBJCEBVF@ZRKMJ", test.verschluesseln(key,
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
     }
 }
