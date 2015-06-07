80edf40595710456999b3d5bec429a12faffe5dd
wagnst <mail@steffenwagner.com>
2015-06-07 19:10:02 +0200

    initialized classes for XOR JUnit tests

---
+++ src/de/wagnst/tpe/exercise/tests/TestXORDecode.java
@@ -0,0 +1,11 @@
+package de.wagnst.tpe.exercise.tests;
+
+import org.junit.Test;
+
+public class TestXORDecode {
+
+    @Test
+    public void testEntschluesseln() throws Exception {
+
+    }
+}
---
+++ src/de/wagnst/tpe/exercise/tests/TestXOREncode.java
@@ -0,0 +1,11 @@
+package de.wagnst.tpe.exercise.tests;
+
+import org.junit.Test;
+
+public class TestXOREncode {
+
+    @Test
+    public void testVerschluesseln() throws Exception {
+
+    }
+}
